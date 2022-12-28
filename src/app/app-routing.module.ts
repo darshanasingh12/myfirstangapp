d2023464d7ac09639b3e436388c6ef5768802b0d
Darshana <darshanasingh1202@gmail.com>
2022-12-28 14:09:46 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthguardGuard } from './authguard.guard';
+import { AloginComponent } from './alogin/alogin.component';
+import { AdashboardComponent } from './adashboard/adashboard.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+{ path: 'home', component: HomeComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+{ path: 'dashboard', component: DashboardComponent,canActivate: [AuthguardGuard] },
+{ path: 'alogin', component:AloginComponent},
+{ path: 'adashboard', component: AdashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AloginComponent } from './alogin/alogin.component';
+import { CommonModule } from '@angular/common';
+import { AdashboardComponent } from './adashboard/adashboard.component';;
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { AnavComponent } from './anav/anav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    AloginComponent,
+    AdashboardComponent,
+    MainNavComponent,
+    AnavComponent 
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
+ 
+
   providers: [],
   bootstrap: [AppComponent]
 })
